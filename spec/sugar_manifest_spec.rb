9bdea4844ec6319f6e1231f54c161cd9c49de19d
Soon Van <svan@upcurvecloud.com>
2018-12-21 18:45:28 -0500

    Add tests to cover summary, install_def methods

--- spec/sugar_manifest_spec.rb
+++ spec/sugar_manifest_spec.rb
@@ -25,5 +25,47 @@ describe SugarManifest do
 ")
     end
   end
+
+  describe ".summary" do
+    it "creates summary block" do
+      expect(@manifester.summary).to eq("$author = 'Soon Van';
+$description = '';
+$key = 'SV-';
+$package_name = '';
+$version = 0.1;
+
+")
+    end
+  end
+
+  describe ".install_defs" do
+    describe "given no lines" do
+      it "creates placeholder copydef block" do
+        expect(@manifester.install_defs).to eq("$installdefs = [
+    'id' => strtolower(preg_replace('/([^a-zA-Z0-9])/', '-', $key.'_'.$package_name)),
+    'copy' => [
+        [
+            'from' => '<basepath>',
+            'to' => '',
+        ],
+    ]
+];
+")
+      end
+    end
+
+    describe "given list of files" do
+      it "creates copydef block with those files" do
+        file_list = "list-of-filenames-as-prepared-by-generate_copy_lines"
+
+        expect(@manifester.install_defs(file_list)).to eq("$installdefs = [
+    'id' => strtolower(preg_replace('/([^a-zA-Z0-9])/', '-', $key.'_'.$package_name)),
+    'copy' => [
+list-of-filenames-as-prepared-by-generate_copy_lines    ]
+];
+")
+      end
+    end
+  end
 end
 
