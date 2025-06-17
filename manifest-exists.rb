cae9bd8cf68fea3cf403cbfb8ed13a97edac8526
Soon Van <cog@randomecho.com>
2025-06-17 11:19:31 -0400

    Fix regex for multiple spaces in manifest name

--- manifest-exists.rb
+++ manifest-exists.rb
@@ -54,9 +54,10 @@ def check_valid_php(source_dir, filename)
 end
 
 def get_package_details(manifest)
-  regex_package_name = /'name' => '([\w\d -.']+)'/
-  regex_version = /'version' => '([\w\d .']+)'/
+  regex_package_name = /'name' => +'([\w\d -.']+)'/
+  regex_version = /'version' => +'([\w\d .']+)'/
   package_name_line = File.readlines(manifest).grep(regex_package_name)
+
   package_name = package_name_line[0].match(regex_package_name)
 
   if package_name.nil?
